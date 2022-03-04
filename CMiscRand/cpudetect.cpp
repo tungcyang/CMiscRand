b98115fffa431970899b9d1126c82c9a6a36dccc
tungcyang <tungcyang@gmail.com>
2022-03-03 16:22:49 -0800

    Add files via upload

--- CMiscRand/cpudetect.cpp
+++ CMiscRand/cpudetect.cpp
@@ -3,21 +3,6 @@
 #include "cpudetect.h"
 
 
-#if 0
-void	__cdecl	sLargerRand(unsigned long _Seed)
-{
-	uLargerRandSeed = _Seed;
-}
-
-long		__cdecl	LargerRand()
-{
-	// We no longer compute ((uLargerRandSeed = uLargerRandSeed * 214013L + 2531011L) >> 16) & 0x7fff;
-	// we only evalute the linear congruential generator and the users need to apply the
-	// right shift and the bitmask.
-	return (long)(uLargerRandSeed = uLargerRandSeed * 214013L + 2531011L);
-}
-#endif
-
 // isIntel() returns 1 if the code is executed on an Intel CPU, 0 otherwise.  Please refer to
 //     https://en.wikipedia.org/wiki/CPUID
 // for more information.
